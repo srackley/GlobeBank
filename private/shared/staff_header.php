5d1e644770597893574a2e9737eceffa55720073
srackley <shelbytrackley@gmail.com>
2018-02-06 15:37:59 -0500

    style, include, require

---
+++ private/shared/staff_header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+  </head>
+
+  <body>
+    <header>GBI Staff Area</header>
+    <nav>
+      <ul>
+        <li><a href='index.php'>Menu</a></li>
+      </ul>
+    </nav>
